0f5152363326f7117992ee143c0672934720e866
nouboussi junior <nouboussijunior@gmail.com>
2018-11-13 18:11:51 +0100

    Ready To Go Live

--- config/assets/production.js
+++ config/assets/production.js
@@ -22,8 +22,9 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/ng-file-upload/ng-file-upload.min.js',
+        'public/lib/ng-file-upload-shim/ng-file-upload-shim.min.js',
         // endbower
       ]
     },
